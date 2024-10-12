de06376a396cc7e6cd758881abaaecef8f1b0da3
WillyMJ04 <wmoralesj2@miumg.edu.gt>
2024-10-12 16:36:42 -0600

    put
    añadiendo spring security

--- project_io/src/main/java/com/example/project/project_io/config/SecurityConfig.java
+++ project_io/src/main/java/com/example/project/project_io/config/SecurityConfig.java
@@ -1,8 +1,13 @@
 package com.example.project.project_io.config;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -11,7 +16,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(@org.jetbrains.annotations.NotNull HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(@NotNull HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
@@ -22,5 +27,16 @@ public class SecurityConfig {
             .httpBasic(withDefaults()); // Usar la configuración por defecto para autenticación básica
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+            .username("admin")
+            .password("admin123")
+            .roles("USER")
+            .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }

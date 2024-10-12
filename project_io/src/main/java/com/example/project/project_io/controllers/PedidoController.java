8c53a312156dc51d351312476b5b3500a29c587d
WillyMJ04 <wmoralesj2@miumg.edu.gt>
2024-10-11 21:53:48 -0600

    put
    update

--- project_io/src/main/java/com/example/project/project_io/controllers/PedidoController.java
+++ project_io/src/main/java/com/example/project/project_io/controllers/PedidoController.java
@@ -10,7 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
+//coment
 @Validated
 @RestController
 @RequestMapping("/pedidos")
